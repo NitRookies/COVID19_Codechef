d30a398e3dc3003a8b0d8344d2a19bdff56d217d
Arijit Saha Ray <52858374+Arijitsaharay@users.noreply.github.com>
2020-05-07 20:08:23 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -4,13 +4,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/index.css">
+
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+
+
+
 </head>
-<style>
-  body{
-  }
-</style>
+
 <body  >
-	<script>  var div = document.createElement('div');  div.className = 'fb-customerchat';  div.setAttribute('page_id', '113682860163746');  div.setAttribute('ref', '');  document.body.appendChild(div);  window.fbMessengerPlugins = window.fbMessengerPlugins || {    init: function () {      FB.init({        appId            : '1678638095724206',        autoLogAppEvents : true,        xfbml            : true,        version          : 'v3.3'      });    }, callable: []  };  window.fbAsyncInit = window.fbAsyncInit || function () {    window.fbMessengerPlugins.callable.forEach(function (item) { item(); });    window.fbMessengerPlugins.init();  };  setTimeout(function () {    (function (d, s, id) {      var js, fjs = d.getElementsByTagName(s)[0];      if (d.getElementById(id)) { return; }      js = d.createElement(s);      js.id = id;      js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";      fjs.parentNode.insertBefore(js, fjs);    }(document, 'script', 'facebook-jssdk'));  }, 0);</script>
+<div id="ais-concierge"></div><script src="https://cdn.aisoftware.com/concierge/index.js"
+id="ais-concierge-script" concierge-token="4e8fe09d-8cef-4af7-8fcd-2bcb8852d4e8"></script>
 <div class="navbar">
   <a class="active" href="index.php">Home</a>
   <div class="dropdown">
@@ -37,8 +40,8 @@
     </button>
     <div class="dropdown-content">
       <a href="predictor.php">Excel Data</a>
-      <a href="https://github.com/NitRookies/COVID19_Codechef/blob/Predictor/COVID_19_Prediction.ipynb" target="_blank">Python Data(1)</a>
-      <a href="https://colab.research.google.com/drive/191P1gxWW1FfjY0Kdbvany1HDaDNiZES1" target="_blank">Python Data(2)</a>
+      <a href="asia.php">Asia</a>
+      <a href="world.php">World</a>
     </div>
   </div>
   <div class="dropdown">
@@ -46,8 +49,8 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="location.php">COVID19 Cases</a>
-      <a href="inhosp.php">Nearest Hospital(India)</a>
+      <a href="https://27mbjlivdgqin4iorizvkg-on.drv.tw/covid/location2.html">COVID19 Cases</a>
+      <a href="https://27mbjlivdgqin4iorizvkg-on.drv.tw/covid/inhosp.html">Nearest Hospital(India)</a>
       <a href="whosp.php">Nearest Hospital(USA)</a>
     </div>
   </div>
@@ -57,14 +60,25 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/media-resources/news"target="_blank">World Health Organization</a>
-      <a href="https://economictimes.indiatimes.com/topic/COVID-19"target="_blank">The Economic Times</a>
+      <a href="who.php">World Health Organization</a>
+      <a href="news.php">India Today</a>
  </a>
     </div>
   </div>
+  <a href="risk.php">Risk Scan</a>
   <a href="help.php">Helping Hand</a>
 </div>
 
-
+<script type="application/javascript" src="https://sdki.truepush.com/sdk/v2.0.2/app.js" async></script>
+<script>
+var truepush = window.truepush || [];
+truepush.push(function(){
+    truepush.Init({
+        id: "5e9487ff38a65d0ddd2e90d1"
+        }, function(error){
+          if(error) console.error(error);
+        })
+    })
+</script>
 </body>
 </html>
